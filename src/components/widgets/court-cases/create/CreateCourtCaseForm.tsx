96d75faa43bd2eff920a5c3465f2502c211c0f3f
vvmelnik <vvmelnik@quasarpro.ru>
2025-12-20 22:42:13 +0300

    refactor: create useCreateCourt

--- src/components/widgets/court-cases/create/CreateCourtCaseForm.tsx
+++ src/components/widgets/court-cases/create/CreateCourtCaseForm.tsx
@@ -1,81 +1,26 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useParams, useRouter } from 'next/navigation';
-import React, { startTransition, useActionState, useEffect } from 'react';
-import { useFieldArray, useForm } from 'react-hook-form';
-import { toast } from 'sonner';
+import { useParams } from 'next/navigation';
 
+import { useCreateCourt } from '@/components/widgets/court-cases/create/hooks/use-create-court';
 import { ContactsSection } from '@/components/widgets/court-cases/create/sections/ContactsSection';
 import { InfoSection } from '@/components/widgets/court-cases/create/sections/InfoSection';
 import { PartiesSection } from '@/components/widgets/court-cases/create/sections/PartiesSection/PartiesSection';
 import { PartyRow } from '@/components/widgets/court-cases/create/sections/PartiesSection/PartyRow';
-import {
-	type TFormCreateCourtCase,
-	createCourtCaseSchema
-} from '@/components/widgets/court-cases/create/validate/schema';
 
 import { SectionCard } from '@/ui/SectionCard';
 
-import { createCourtCaseAction } from '@/actions/create-court-case.action';
-import { REAL_ESTATE_URL } from '@/routes';
-
-const initialState = { error: undefined, success: false };
-
 export function CreateCourtCaseForm() {
-	const [state, action, isPending] = useActionState(createCourtCaseAction, initialState);
-
 	const { uuid } = useParams<{ uuid: string }>();
-	const router = useRouter();
 	const {
-		register,
-		control,
-		handleSubmit,
-		formState: { errors }
-	} = useForm<TFormCreateCourtCase>({
-		resolver: zodResolver(createCourtCaseSchema),
-		defaultValues: {
-			caseParties: [
-				{ role: 'applicant', fio: '' },
-				{ role: 'respondent', fio: '' }
-			],
-			courtCase: {
-				name: '',
-				instance: '',
-				nextHearingDate: new Date().toISOString(),
-				status: 'in_progress',
-				judgeFio: '',
-				email: '',
-				phone: ''
-			}
-		}
-	});
-
-	const { fields, append, remove } = useFieldArray({
-		control,
-		name: 'caseParties'
-	});
-
-	// возвращаемся на просмотр ПОСЛЕ успешного сохранения
-	useEffect(() => {
-		if (state.success) {
-			toast.success('Данные успешно сохранены');
-			router.push(`${REAL_ESTATE_URL}/${uuid}/court-cases`);
-		} else if (state.error) {
-			toast.error('Ошибка создания судебного дела. Проверьте дату заседания');
-		}
-	}, [state.success, state.error, router, uuid]);
-
-	const onSubmit = async (submitData: TFormCreateCourtCase) => {
-		startTransition(() => {
-			action({
-				uuid: uuid,
-				courtCase: {
-					...submitData.courtCase
-				},
-				caseParties: [...submitData.caseParties]
-			});
-			state.error = '';
-		});
-	};
+		form: {
+			register,
+			handleSubmit,
+			control,
+			formState: { errors }
+		},
+		fieldsForm: { append, fields, remove },
+		onSubmit,
+		isPending
+	} = useCreateCourt({ uuid });
 
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
---
+++ src/components/widgets/court-cases/create/hooks/use-create-court.ts
@@ -0,0 +1,79 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import { startTransition, useActionState, useEffect } from 'react';
+import { useFieldArray, useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+
+import {
+	type TFormCreateCourtCase,
+	createCourtCaseSchema
+} from '@/components/widgets/court-cases/create/validate/schema';
+
+import { createCourtCaseAction } from '@/actions/create-court-case.action';
+import { REAL_ESTATE_URL } from '@/routes';
+import type { Uuid } from '@/types/common';
+
+const initialState = { error: undefined, success: false };
+
+interface IUseCreateCourtOptions {
+	uuid: Uuid;
+}
+
+export function useCreateCourt({ uuid }: IUseCreateCourtOptions) {
+	const [state, action, isPending] = useActionState(createCourtCaseAction, initialState);
+
+	const router = useRouter();
+
+	const form = useForm<TFormCreateCourtCase>({
+		resolver: zodResolver(createCourtCaseSchema),
+		defaultValues: {
+			caseParties: [
+				{ role: 'applicant', fio: '' },
+				{ role: 'respondent', fio: '' }
+			],
+			courtCase: {
+				name: '',
+				instance: '',
+				nextHearingDate: new Date().toISOString(),
+				status: 'in_progress',
+				judgeFio: '',
+				email: '',
+				phone: ''
+			}
+		}
+	});
+
+	const fieldsForm = useFieldArray({
+		control: form.control,
+		name: 'caseParties'
+	});
+
+	// возвращаемся на просмотр ПОСЛЕ успешного сохранения
+	useEffect(() => {
+		if (state.success) {
+			toast.success('Данные успешно сохранены');
+			router.push(`${REAL_ESTATE_URL}/${uuid}/court-cases`);
+		} else if (state.error) {
+			toast.error('Ошибка создания судебного дела. Проверьте дату заседания');
+		}
+	}, [state.success, state.error, router, uuid]);
+
+	const onSubmit = async (submitData: TFormCreateCourtCase) => {
+		startTransition(() => {
+			action({
+				uuid: uuid,
+				courtCase: {
+					...submitData.courtCase
+				},
+				caseParties: [...submitData.caseParties]
+			});
+		});
+	};
+
+	return {
+		form,
+		fieldsForm,
+		onSubmit,
+		isPending
+	};
+}
